dddd3703789d9700f5d40b52cd6d208a9dedea5c
alipiapia <124910168@qq.com>
2018-12-14 13:26:07 +0800

    UrlPrefix

--- static/plugs/ueditor/ueditor.config.js
+++ static/plugs/ueditor/ueditor.config.js
@@ -278,11 +278,11 @@
 
         //autoHeightEnabled
         // 是否自动长高,默认true
-        ,autoHeightEnabled:false
+        //,autoHeightEnabled:false
 
         //scaleEnabled
         //是否可以拉伸长高,默认true(当开启时，自动长高失效)
-        //,scaleEnabled:false
+        ,scaleEnabled:true
         //,minFrameWidth:800    //编辑器拖动时最小宽度,默认800
         //,minFrameHeight:220  //编辑器拖动时最小高度,默认220
 
