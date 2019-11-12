393a4906c3f671b436969223a23c13991f5eabb0
alipiapia <124910168@qq.com>
2019-11-12 09:41:08 +0800

    Member/login

--- application/api/controller/Member.php
+++ application/api/controller/Member.php
@@ -130,17 +130,17 @@ class Member extends BasicApi
             $this->error('登录密码错误，请重新输入');            
         }
 
-        $codeInfo = $this->SmsLog->getOneDarry($map, 'code,create_at');
-        if(!$codeInfo){
-            $this->error('验证码错误');
-        }
-        $timeDiff = floor(time() - $codeInfo['create_at']);
-        if(($timeDiff / 60) >= 60){
-            $this->error('验证码已过期');
-        }
-        if($codeInfo['code'] != $code){
-            $this->error('验证码错误');
-        }
+        // $codeInfo = $this->SmsLog->getOneDarry($map, 'code,create_at');
+        // if(!$codeInfo){
+        //     $this->error('验证码错误');
+        // }
+        // $timeDiff = floor(time() - $codeInfo['create_at']);
+        // if(($timeDiff / 60) >= 60){
+        //     $this->error('验证码已过期');
+        // }
+        // if($codeInfo['code'] != $code){
+        //     $this->error('验证码错误');
+        // }
 
         //增加用户保密协议文本内容
         $mem['accept_content'] = sysconf('accept_content');
@@ -183,17 +183,17 @@ class Member extends BasicApi
             $this->error('新密码不能与原密码一致，请重新输入');            
         }
 
-        // $codeInfo = $this->SmsLog->getOneDarry($map, 'code,create_at');
-        // if(!$codeInfo){
-        //     $this->error('验证码错误');
-        // }
-        // $timeDiff = floor(time() - $codeInfo['create_at']);
-        // if(($timeDiff / 60) >= 60){
-        //     $this->error('验证码已过期');
-        // }
-        // if($codeInfo['code'] != $code){
-        //     $this->error('验证码错误');
-        // }
+        $codeInfo = $this->SmsLog->getOneDarry($map, 'code,create_at');
+        if(!$codeInfo){
+            $this->error('验证码错误');
+        }
+        $timeDiff = floor(time() - $codeInfo['create_at']);
+        if(($timeDiff / 60) >= 60){
+            $this->error('验证码已过期');
+        }
+        if($codeInfo['code'] != $code){
+            $this->error('验证码错误');
+        }
 
         // 更新登录信息
         $up = $this->member->where($map)->update([
